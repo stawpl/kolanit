cd5191cc9d11cd7090bbfd8d79cf331184d4c9b4
stawpl <65124631+stawpl@users.noreply.github.com>
2020-05-19 21:12:03 +0300

    Add files via upload

---
+++ 224/cdn/js/geo/pl2.js
@@ -0,0 +1,19 @@
+(function() {
+    'use strict';
+
+    window.countryList = {
+        pl: {
+            name: 'Polska',
+            code: 'pl',
+            price: 179,
+            oldPrice: 358,
+            labelPrice: 'zł',
+            phoneHelper: 'Na przykład, +48 123456789',
+            nameHelper: 'Tom Czerniak',
+            nameError: 'Imię (wymagane)',
+            phoneError: 'Błędnie podany numer',
+            countryError: 'Kraj (wymagany)',
+            isDefault: true
+        }
+    };
+})();
